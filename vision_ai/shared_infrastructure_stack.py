2c3266aa2747bdd6c087d1f22898a54bdbe298b1
Zainab Alobed <zainabh647@gmail.com>
2025-12-02 21:46:59 +0300

    fix the bucket name

--- vision_ai/shared_infrastructure_stack.py
+++ vision_ai/shared_infrastructure_stack.py
@@ -20,8 +20,7 @@ class SharedInfrastructureStack(Stack):
         # ==========================================
         self.investigation_bucket = s3.Bucket(
             self, "InvestigationBucket",
-            bucket_name="vision-rt-investigation-system",
-            versioned=True,
+            bucket_name=f"vision-investigation-system-{self.account}",              versioned=True,
             encryption=s3.BucketEncryption.S3_MANAGED,
             removal_policy=RemovalPolicy.RETAIN,
             cors=[s3.CorsRule(
